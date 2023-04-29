b7ab27efb70f5fc50c0b6a421f8373efc734d4ef
Adrian Berger <adrian.berger@bedag.ch>
2023-04-29 15:16:55 +0200

    Add 404 error page

---
+++ app/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img src="/images/logos/tellbow_cricle_crop.png" class="mt-5 w-2" />
+    <div class="text-primary-500 font-bold text-7xl mt-2">404</div>
+
+    <div class="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+      <p>Die Seite wurde nicht gefunden.</p>
+      <a href="/" class="text-primary">> Startseite</a>
+    </div>
+  </div>
+</template>
