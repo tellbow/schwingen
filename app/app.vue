88735ab99f7c4c398ace3168e95985468a07da0e
Adrian Berger <adrian.berger@bedag.ch>
2023-05-13 19:14:17 +0200

    Prepare mobile view

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,19 @@
 <script lang="ts" setup>
 const ready = true;
+
+const layout =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+    ? "mobile"
+    : "default";
 </script>
 
 <template>
   <div>
     <Toast />
     <Toast position="bottom-right" group="br" />
-    <NuxtLayout v-show="ready">
+    <NuxtLayout v-show="ready" :name="layout">
       <NuxtLoadingIndicator />
       <NuxtPage />
     </NuxtLayout>
---
+++ app/layouts/mobile.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const appConfig = useAppConfig();
+// Ensures that the pocketbase url is set to the current host
+const pb = usePocketbase();
+onMounted(() => {
+  const url = window.location.host;
+  if (!url.includes("localhost")) {
+    pb.baseUrl = "https://" + url;
+  }
+});
+</script>
+
+<template>
+  <div class="layout-empty">
+    <Header />
+    <slot />
+    <Footer />
+    <ScrollTop />
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #eebb90;
+}
+</style>
