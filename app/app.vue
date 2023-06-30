d55145beb8f08b7f7b2d938a95cc5c389b8172c6
adberger <adria.berger94@gmail.com>
2023-06-30 17:37:04 +0200

    Add more meta and seo data

--- app/app.vue
+++ app/app.vue
@@ -12,6 +12,13 @@ useHead({
   title: "Tellbow",
   meta: [{ name: "description", content: "Alles rund ums Schwingen." }],
   htmlAttrs: { lang: "de" },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
 });
 
 useSeoMeta({
@@ -19,7 +26,13 @@ useSeoMeta({
   ogTitle: "Tellbow",
   description: "Alles rund ums Schwingen.",
   ogDescription: "Alles rund ums Schwingen.",
-  ogImage: "https://schwingen.tellbow.ch/images/logos/tellbow.svg",
+  ogImage: "https://schwingen.tellbow.ch/images/logos/tellbow_cricle_crop.png",
+  ogUrl: "https://schwingen.tellbow.ch/wrestler/5h1fij68r6jh8ut",
+  twitterTitle: "Tellbow",
+  twitterDescription: "Alles rund ums Schwingen.",
+  twitterImage:
+    "https://schwingen.tellbow.ch/images/logos/tellbow_cricle_crop.png",
+  twitterCard: "summary",
 });
 </script>
 
