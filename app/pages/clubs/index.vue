d9e0a2bdc49120bc26fbca9205577b90e06a3b9e
Adrian Berger <adria.berger94@gmail.com>
2023-08-07 20:19:32 +0200

    Add map for association

--- app/pages/clubs/index.vue
+++ app/pages/clubs/index.vue
@@ -15,13 +15,22 @@ import maps from "~/public/topo/swiss-maps-modified.json";
 
 const pocketbase = usePocketbase();
 
-const chart = ref();
+const chartCanton = shallowRef();
+const chartAssociation = shallowRef();
 const fullCantonData = ref();
-const associationData = ref();
+const fullAssociationData = ref();
 const clubData = ref();
 const loadingAssociation = ref(true);
 
 onMounted(async () => {
+  const nation: Feature = topojson.feature(
+    maps as unknown as Topology,
+    maps.objects.country as GeometryCollection,
+  ).features[0];
+  const cantons: Array<Feature> = topojson.feature(
+    maps as unknown as Topology,
+    maps.objects.cantons as GeometryCollection,
+  ).features;
   await pocketbase
     .collection("wrestlersByAssociation")
     .getFullList(10 /* batch size */, {
@@ -29,7 +38,158 @@ onMounted(async () => {
       fields: "id,name,abbreviation,wrestlerAmount,wrestlerActive",
     })
     .then((data) => {
-      associationData.value = data;
+      fullAssociationData.value = data;
+
+      const mergedData: any = {};
+
+      // Merge first dataset based on name
+      for (const item of cantons) {
+        if (item.properties) {
+          const name = item.properties.name;
+          mergedData[name] = { ...mergedData[name], ...item };
+        }
+      }
+
+      // Merge second dataset based on name
+      for (const item of fullAssociationData.value) {
+        const abbreviation = item.abbreviation;
+        switch (abbreviation) {
+          case "BKSV":
+            mergedData.Bern = { ...mergedData.Bern, ...item };
+            break;
+          case "ISV": {
+            const isv = [
+              "Luzern",
+              "Nidwalden",
+              "Obwalden",
+              "Schwyz",
+              "Tessin",
+              "Uri",
+              "Zug",
+            ];
+            isv.forEach((canton) => {
+              mergedData[canton].wrestlerActive = item.wrestlerActive;
+            });
+            break;
+          }
+          case "NOSV": {
+            const nosv = [
+              "Appenzell Ausserrhoden",
+              "Appenzell Innerrhoden",
+              "Glarus",
+              "Graubünden",
+              "Schaffhausen",
+              "St. Gallen",
+              "Thurgau",
+              "Zürich",
+            ];
+            nosv.forEach((canton) => {
+              mergedData[canton].wrestlerActive = item.wrestlerActive;
+            });
+            break;
+          }
+          case "NWSV": {
+            const nwsv = ["Aargau", "Basel-Stadt", "Baselland", "Solothurn"];
+            nwsv.forEach((canton) => {
+              mergedData[canton].wrestlerActive = item.wrestlerActive;
+            });
+            break;
+          }
+          case "SWSV": {
+            const swsv = [
+              "Fribourgeoise",
+              "Genevoise",
+              "Jura",
+              "Neuchâteloise",
+              "Valaisanne",
+              "Vaudoise",
+            ];
+            swsv.forEach((canton) => {
+              mergedData[canton].wrestlerActive = item.wrestlerActive;
+            });
+            break;
+          }
+        }
+      }
+
+      // Convert the merged object back to an array if needed
+      const mergedArray = Object.values(mergedData);
+
+      const config = {
+        type: "choropleth",
+        data: {
+          labels: mergedArray.map((d: any) => d.properties.name),
+          datasets: [
+            {
+              label: "Teilverbände",
+              outline: nation,
+              showOutline: true,
+              data: mergedArray.map((d: any) => ({
+                feature: d,
+                value: d.wrestlerActive,
+              })),
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            },
+            datalabels: {
+              align: "center",
+              color: "#000",
+              font: {
+                size: "12",
+                weight: "bold",
+              },
+              textShadowBlur: 10,
+              textShadowColor: "#000",
+              formatter: (v: any) => {
+                return (
+                  v.feature.properties.name + ": " + v.feature.wrestlerActive
+                );
+              },
+              display: function (context: any) {
+                return context.active;
+              },
+            },
+          },
+          scales: {
+            projection: {
+              axis: "x",
+              projection: "mercator",
+              projectionOffset: [-30, 0],
+            },
+            color: {
+              axis: "x",
+              quantize: 10,
+              interpolate: "oranges",
+            },
+          },
+        },
+      };
+
+      Chart.register(
+        ChoroplethController,
+        GeoFeature,
+        ColorScale,
+        ProjectionScale,
+        ChartDataLabels,
+      );
+      const canvasAssociation = document.getElementById(
+        "canvasAssociation",
+      ) as HTMLCanvasElement;
+      const ctxAssociation = canvasAssociation.getContext("2d");
+      if (
+        !ctxAssociation ||
+        !(ctxAssociation instanceof CanvasRenderingContext2D)
+      ) {
+        navigateTo("/error");
+      } else {
+        // chart.value = new ChoroplethChart(ctx, config as any);
+        chartAssociation.value = new Chart(ctxAssociation, config as any);
+      }
       loadingAssociation.value = false;
     });
   await pocketbase
@@ -40,14 +200,6 @@ onMounted(async () => {
     })
     .then((data) => {
       fullCantonData.value = data;
-      const nation: Feature = topojson.feature(
-        maps as unknown as Topology,
-        maps.objects.country as GeometryCollection,
-      ).features[0];
-      const cantons: Array<Feature> = topojson.feature(
-        maps as unknown as Topology,
-        maps.objects.cantons as GeometryCollection,
-      ).features;
 
       const mergedData: any = {};
 
@@ -120,6 +272,13 @@ onMounted(async () => {
             },
             datalabels: {
               align: "center",
+              color: "#000",
+              font: {
+                size: "12",
+                weight: "bold",
+              },
+              textShadowBlur: 10,
+              textShadowColor: "#000",
               formatter: (v: any) => {
                 return (
                   v.feature.properties.name + ": " + v.feature.wrestlerActive
@@ -135,17 +294,12 @@ onMounted(async () => {
               axis: "x",
               projection: "mercator",
               projectionOffset: [-30, 0],
-              // projectionOffset: [8.3, 46.8],
-              // padding: [975 / 2, 610 / 2],
             },
-            /* color: {
-          axis: "x",
-          quantize: 5,
-          legend: {
-            position: "bottom-right",
-            align: "right",
-          },
-        }, */
+            color: {
+              axis: "x",
+              quantize: 10,
+              interpolate: "oranges",
+            },
           },
         },
       };
@@ -157,16 +311,22 @@ onMounted(async () => {
         ProjectionScale,
         ChartDataLabels,
       );
-      const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-      const ctx = canvas.getContext("2d");
-      if (!ctx || !(ctx instanceof CanvasRenderingContext2D)) {
+      const canvasCanton = document.getElementById(
+        "canvasCanton",
+      ) as HTMLCanvasElement;
+      const ctxCanton = canvasCanton.getContext("2d");
+      if (!ctxCanton || !(ctxCanton instanceof CanvasRenderingContext2D)) {
         navigateTo("/error");
       } else {
         // chart.value = new ChoroplethChart(ctx, config as any);
-        chart.value = new Chart(ctx, config as any);
+        chartCanton.value = new Chart(ctxCanton, config as any);
       }
     });
 });
+
+const onTabOpen = () => {};
+
+const onTabClose = () => {};
 
 const onSubTabOpen = async (event: { index: string | number }) => {
   await pocketbase
@@ -192,18 +352,28 @@ function getCantons(associationId: string) {
     <div class="flex justify-center">
       <div class="h-fit w-screen md:w-4">
         <p class="text-xl font-bold mt-1 md:mt-2 mb-1 md:mb-2 text-center">
+          Anzahl Schwinger pro Teilverband
+        </p>
+        <canvas id="canvasAssociation" ref="canvasAssociation"></canvas>
+        <p class="text-xl font-bold mt-1 md:mt-2 mb-1 md:mb-2 text-center">
           Anzahl Schwinger pro Kanton
         </p>
-        <canvas id="canvas" ref="canvas"></canvas>
+        <canvas id="canvasCanton" ref="canvasCanton"></canvas>
       </div>
     </div>
     <div
       class="justify-content-center align-content-center display: flex flex-wrap fill-height mt-3 md:mt-6"
     >
       <ProgressSpinner v-if="loadingAssociation" />
-      <Accordion v-else lazy class="w-12 sm:w-10 md:w-8 lg:w-6 xl:w-4">
+      <Accordion
+        v-else
+        lazy
+        class="w-12 sm:w-10 md:w-8 lg:w-6 xl:w-4"
+        @tab-click="onTabOpen()"
+        @tab-close="onTabClose()"
+      >
         <AccordionTab
-          v-for="association in associationData"
+          v-for="association in fullAssociationData"
           :key="association.id"
           :header="
             association.abbreviation +
