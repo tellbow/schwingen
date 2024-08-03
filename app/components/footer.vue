1dd517441a7acfcf17c8883d2d4d996c82c880ae
Adrian Berger <adrian.berger@bedag.ch>
2024-08-03 18:36:14 +0200

    chore: Add source (github) at footer

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <p class="m-5 text-center">©Tellbow</p>
+    <p class="m-5 text-center">
+      ©Tellbow
+      <a
+        href="https://github.com/tellbow/schwingen"
+        target="_blank"
+        class="pi pi-github"
+      ></a>
+    </p>
   </div>
 </template>
